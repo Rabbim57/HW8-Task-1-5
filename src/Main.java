cbfe414e1ea75a0391315220893147c856ac25d4
Rabbim57 <Rabbim1998@mail.ru>
2023-08-22 22:59:37 +0300

    Проверьте

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,56 @@
 public class Main {
     public static void main(String[] args) {
-
-
         // Task 1
-        System.out.println();
-        double[] number = {1, 2, 3};
-        number[0] = 1.57;
-        number[1] = 7.654;
-        number[2] = 9.986;
-        for (int meaning = 0; meaning < number.length; meaning++) {
-            System.out.println("Значение " + meaning + " равна " + number[meaning]);
+        System.out.println("Task 1");
+        int[] numberOne = new int[3];
+        numberOne[0] = 1;
+        numberOne[1] = 2;
+        numberOne[2] = 3;
+        for (int i = 0; i < 3; i++)
+            System.out.println(numberOne[i]);
 
-        }
         System.out.println();
-        // Task 2
-        double[] numberTwo = {1, 2, 3};
+        System.out.println("Task 1.1");
+        double[] numberTwo = new double[3];
         numberTwo[0] = 1.57;
         numberTwo[1] = 7.654;
         numberTwo[2] = 9.986;
-        for (int meaningTwo = 0; meaningTwo < number.length; meaningTwo++) {
-            System.out.print(number[meaningTwo] + " ");
+        for (int i = 0; i < 3; i++)
+            System.out.println(numberTwo[i]);
+
+        System.out.println();
+        System.out.println("Task 1.2");
+
+        double[] numberThree = {1.55, 2.77, 3.45};
+        for (int d = 0; d < numberThree.length; d++) {
+            System.out.println(numberThree[d]);
         }
+
         System.out.println();
+        System.out.println("Task 2");
+
+        int[] numberFour = {1, 2, 3};
+        for (int d = 0; d < numberFour.length; d++) {
+            System.out.println(numberFour[d] + " ");
+        }
         // Task 3
-        double[] numberThree = {3, 2, 1};
-        numberThree[0] = 1.57;
-        numberThree[1] = 7.654;
-        numberThree[2] = 9.986;
-        for (int meaningThree = 0; meaningThree < number.length; meaningThree++)
-            if (meaningThree == numberThree.length - 1) {
-                System.out.println(numberThree[meaningThree] + " ");
-            }
         System.out.println();
+        System.out.println("Task 3");
+
+
+
+
+
         // Task 4
-        double[] numberFour = {3, 2, 1};
-        numberFour[0] = 1.57;
-        numberFour[1] = 7.654;
-        numberFour[2] = 9.986;
-        for (int meaningFour = 0; meaningFour <= number.length; meaningFour++) {
-                System.out.println(numberFour[meaningFour] + 1 + " ");
-            }
+        System.out.println();
+        System.out.println("Task 4");
+
+        int [] numberFive = {1, 2, 3};
+        numberFive[0] = 1;
+        numberFive[1] = 7;
+        numberFive[2] = 9;
+        for (int i = 0; i < numberFive.length; i ++)
+                System.out.println( numberFive [i] + 1);
 
 
 
